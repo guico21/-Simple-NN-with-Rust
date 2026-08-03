a45517bac654c420ccbf794ca73fbaf8c762f8be
guico21 <156781946+guico21@users.noreply.github.com>
2026-08-03 16:13:36 +0200

    Added backward basic step in layer

--- src/layer.rs
+++ src/layer.rs
@@ -64,6 +64,47 @@ impl Layer {
         self.last_a = Some(a.clone());
         a
     }
+
+    pub fn backward(&mut self, d_out: &[f32]) -> Vec<f32> {
+        // get caches. there is a panic if forward was not called before
+        // thus caches will be None.
+        // WARN must improve the erro handling here
+        let z = match &self.last_z {
+            Some(z) => z,
+            None => panic!("Forward not called due to last_z."),
+        };
+        let input = match &self.last_input {
+            Some(z) => z,
+            None => panic!("Forward not called due to last_input"),
+        };
+        // activation backward
+        let d_z = match self.activation {
+            Activation::ReLU => d_out
+                .iter()
+                .zip(z.iter())
+                .map(|(&do_val, &z_val)| if z_val > 0.0 { do_val } else { 0.0 })
+                .collect(),
+            Activation::Linear => d_out.to_vec(),
+        };
+        // weight gradient
+        let n_in = self.w.col;
+        let n_out = self.w.row;
+        let mut w_grad_data = vec![0.0; n_out * n_in];
+        for r in 0..n_out {
+            for c in 0..n_in {
+                w_grad_data[r * n_in + c] = d_z[r] * input[c];
+            }
+        }
+        self.w_grad = Some(Matrix::new(n_out, n_in, Some(w_grad_data)));
+        // bias gradient
+        self.b_grad = Some(d_z.clone());
+        // gradient with respect the input (for previous layer)
+        let d_prev = self
+            .w
+            .transpose_dot_vector(&d_z)
+            .expect("Dimension mismatch in backward.");
+        d_prev
+    }
 }
 
 #[cfg(test)]
